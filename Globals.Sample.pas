04d199ae5d7b438bdecfd6dadc2b45da3a847dfa
Geoff <fork@tysontechnology.com.au>
2020-07-05 15:21:22 +1000

    Added globals.sample.pas to show example globals.pas

---
+++ Globals.Sample.pas
@@ -0,0 +1,21 @@
+unit Globals;
+
+interface
+
+// Please update all these constants to yours and rename this file to globals.pas
+
+const google_clientid = 'XXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXX.apps.googleusercontent.com';
+const google_clientsecret = 'XXXXXXXXXXXXXXXXXXXXXXX';
+const google_clientaccount = 'xxxxxxx@gmail.com';
+
+const microsoft_clientid = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX';
+const microsoft_clientsecret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const microsoft_clientaccount = 'xxxxxxxxx@hotmail.com';
+
+const clientsendtoaddress  = 'youremail@example.com';
+const clientname = 'John Smith';
+
+
+implementation
+
+end.
